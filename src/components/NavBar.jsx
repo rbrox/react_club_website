1996d778d486a2ac7ca5a23c7b8cc79b209c344d
Rishav Bhagat <rishavbhagat@Rishavs-MacBook-Air.local>
2024-03-12 10:42:26 +0530

    background added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="text-white font-semibold">Your Brand</div>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="text-white">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
